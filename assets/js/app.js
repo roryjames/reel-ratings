848f8cc04bfc4f45f7105d95c85f8c4752f74aab
Rory James <rjlanam@gmail.com>
2017-07-07 01:55:32 -0500

    actor search in progress

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,14 +17,21 @@ $(document).ready(function() {
             var results = response.results; //ajax results into variable
             console.log(results);
 
-            for (var i = 0; i < results.length; i++) {
+            for (var i = 0; i < response.results.length; i++) {
+                console.log(response.results[i].media_type);
+            if (response.results[i].media_type === 'person'){
+                for (var i = 0; i < results.length; i++) {
+                    
+                }
+            }
+            else{
+            for (var j = 0; j < results.length; j++) {
+                if (results[j].name === undefined){ //api returns 'name' for TV shows, not 'title'
 
-                if (results[i].name === undefined){ //api returns 'name' for TV shows, not 'title'
-
-                    var str = results[i].title;
+                    var str = results[j].title;
                     str = str.replace(/\s+/g, '-').toLowerCase();
 
-                    var yearFull = results[i].release_date; //takes release date from themoviedb 
+                    var yearFull = results[j].release_date; //takes release date from themoviedb 
                     var year = yearFull.substring(0, 4); //extracts the year
 
                     var movieBox = $('<div>'); //creates div for poster + info
@@ -36,16 +43,16 @@ $(document).ready(function() {
 
                     var img = $('<img>');
                     img.addClass('img-responsive');
-                    if (results[i].poster_path == null){
+                    if (results[j].poster_path == null){
                         img.attr('src', 'assets/media/image_not_found.png');
                     }
                     else{
-                        img.attr("src", "https://image.tmdb.org/t/p/w342//" + results[i].poster_path);
+                        img.attr("src", "https://image.tmdb.org/t/p/w342//" + results[j].poster_path);
                     }
                     img.attr('data-name', str);
-                    img.attr('data-id', results[i].id);
+                    img.attr('data-id', results[j].id);
                     img.attr('data-ratings', str + "&y=" + year);
-                    var information = $('<h4>').html(results[i].title + " ("+ year +")");
+                    var information = $('<h4>').html(results[j].title + " ("+ year +")");
                     posterBox.append(img, information);
 
                     movieBox.append(posterBox);
@@ -54,14 +61,14 @@ $(document).ready(function() {
 
                 }
                 else {
-                    var str = results[i].name;
+                    var str = results[j].name;
                     str = str.replace(/\s+/g, '-').toLowerCase();
 
-                    if (results[i].first_air_date === ""){
+                    if (results[j].first_air_date === ""){
                         var year = "N/A";
                     }
                     else{
-                        var yearFull = results[i].first_air_date; //takes release date from themoviedb 
+                        var yearFull = results[j].first_air_date; //takes release date from themoviedb 
                         var year = yearFull.substring(0, 4); //extracts the year
                     }
                     var movieBox = $('<div>'); //creates div for poster + info
@@ -73,17 +80,17 @@ $(document).ready(function() {
                     var img = $('<img>');
                     img.addClass('img-responsive');
 
-                    if (results[i].poster_path == null){
+                    if (results[j].poster_path == null){
                         img.attr('src', 'assets/media/image_not_found.png');
                     }
                     else{
-                        img.attr("src", "https://image.tmdb.org/t/p/w342//" + results[i].poster_path);
+                        img.attr("src", "https://image.tmdb.org/t/p/w342//" + results[j].poster_path);
                     }
 
                     img.attr('data-name', str);
-                    img.attr('data-id', results[i].id);
+                    img.attr('data-id', results[j].id);
                     img.attr('data-ratings', str + "&y=" + year);
-                    var information = $('<h4>').html(results[i].name + " ("+ year +")");
+                    var information = $('<h4>').html(results[j].name + " ("+ year +")");
                     posterBox.append(img, information);
 
                     movieBox.append(posterBox);
@@ -91,6 +98,8 @@ $(document).ready(function() {
                     $('#moviesHere').append(movieBox);
                 }
             };
+        }
+    };
         });
     });
 });
